fba85f484539d3466dbde4e583c7470de3338f33
kaileenc <91224061+kaileenc@users.noreply.github.com>
2021-12-04 19:22:42 -0800

    Update courseproject.py

--- courseproject.py
+++ courseproject.py
@@ -9,7 +9,7 @@ import seaborn as sns
 
 st.title("Analyzing Characteristics of K-Pop Listeners")
 
-df = pd.read_csv('KPOPDATA.csv')
+df = pd.read_csv('KPOP_DATA.csv')
                    
 #first, clean the data
 
