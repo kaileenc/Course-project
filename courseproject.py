6941a596e11555920fd12de42739d590523915bb
kaileenc <91224061+kaileenc@users.noreply.github.com>
2021-12-04 17:50:43 -0800

    Update courseproject.py

--- courseproject.py
+++ courseproject.py
@@ -10,7 +10,7 @@ import openpyxl as openpyxl
 
 st.title("Analyzing Characteristics of K-Pop Listeners")
 
-df = pd.read_excel("C:\Users\Kaileen\Documents\UCI\"uci fall 2021\Math 10"\KPOPData.xlsx',engine='openpyxl')
+df = pd.read_excel("C:\Users\Kaileen\Documents\UCI\"uci fall 2021\Math 10"\KPOPData.xlsx",engine='openpyxl')
                    
 #first, clean the data
 
